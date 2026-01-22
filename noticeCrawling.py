df621bac588cba39d839d31063614567c11cb8c3
DongGeon <103568083+sondg921@users.noreply.github.com>
2026-01-22 21:47:02 +0900

    Update noticeCrawling.py

--- noticeCrawling.py
+++ noticeCrawling.py
@@ -2,6 +2,18 @@ import bs4
 import pandas as pd
 import requests
 
+from feedgen.feed import FeedGenerator
+fg = FeedGenerator ()
+fg.id ('http://lernfunk.de/media/654321')
+fg.title('Some Testfeed' )
+fg.author({'name': 'John Doe', 'email': 'john@example.de'})
+fg. link( href='http://example.com', rel='alternate' )
+fg. logo ('http://ex.com/logo.jpg')
+fg.subtitle('This is a cool feed!')
+fg. link( href='http://larskiesow.de/test.atom', rel='self' )
+fg. language('en')
+fg.rss_file('rss.xml')
+
 url = "https://www.smu.ac.kr/kor/life/notice.do?srCampus=smu"
 html = requests.get(url).text
 
