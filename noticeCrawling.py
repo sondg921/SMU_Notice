eae8f010cfe2a09ba94e899f92c701d498e4e3f6
DongGeon <103568083+sondg921@users.noreply.github.com>
2026-01-23 11:00:29 +0900

    Update noticeCrawling.py

--- noticeCrawling.py
+++ noticeCrawling.py
@@ -1,22 +1,26 @@
 import bs4
 import pandas as pd
 import requests
-
+import re
 from feedgen.feed import FeedGenerator
+from datetime import datetime
+import pytz
+
 fg = FeedGenerator ()
 fg.id ('http://lernfunk.de/media/654321')
-fg.title('Some Testfeed' )
+fg.title('SMU Notice' )
 fg.author({'name': 'John Doe', 'email': 'john@example.de'})
-fg. link( href='http://example.com', rel='alternate' )
-fg. logo ('http://ex.com/logo.jpg')
+fg.link( href='http://example.com', rel='alternate' )
+fg.logo ('http://ex.com/logo.jpg')
 fg.subtitle('This is a cool feed!')
-fg. link( href='http://larskiesow.de/test.atom', rel='self' )
-fg. language('en')
-fg.rss_file('docs/rss.xml')
+fg.link( href='http://larskiesow.de/test.atom', rel='self' )
+fg.language('en')
+fg.rss_file('rss.xml')
+
+kst = pytz.timezone('Asia/Seoul')
 
 url = "https://www.smu.ac.kr/kor/life/notice.do?srCampus=smu"
 html = requests.get(url).text
-
 soup = bs4.BeautifulSoup(html, "html.parser")
 
 data = []
@@ -30,6 +34,21 @@ for tag in soup.select(css_selector):
     fullUrl = "https://www.smu.ac.kr/kor/life/notice.do" + url
     data.append((head, date, fullUrl))
 
+    fe = fg.add_entry()
+    fe.title(head)
+    fe.link(href=fullUrl)
+
+    article_no_match = re.search(r'articleNo=(\d+)', url)
+    article_no = article_no_match.group(1) if article_no_match else "0"
+    fe.guid(article_no, permalink=False)
+
+    try:
+        dt = datetime.strptime(date, '%Y-%m-%d')
+        fe.pubDate(kst.localize(dt))
+    except:
+        fe.pubDate(datetime.now(kst))
+
 df = pd.DataFrame(data = data, columns = ["Head", "Date", "Url"])
-df['Date'] = pd.to_datetime(df['Date'])
-df.to_csv("docs/notice.csv", index = False)
+df.to_csv("notice.csv", index = False, encoding='utf-8-sig')
+
+fg.rss_file('feed.xml', pretty=True)
